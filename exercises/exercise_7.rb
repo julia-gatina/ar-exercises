20b2b6d0ff5a686fd5ab268d580d7f98befc8f96
Julia <julia.sarandi@gmail.com>
2022-02-02 15:33:10 -0800

    Attempt to create store with name entered by user, display errors to user

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,14 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+#Ask the user for a store name
+puts "Please enter a store name: "
+store = gets.chomp
+
+#Attempt to create a store with the inputted name but leave out the other
+store = Store.create(name: store)
+
+#Display the error messages provided back from ActiveRecord to the user
+store.errors.messages.each do |message|
+  puts "#{message}"
+  end
